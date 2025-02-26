a8ac4c707d366ce5758927db88055767cbbbce7a
Samantha Chu <samasa112233@gmail.com>
2025-02-26 21:29:37 +0800

    add css file and modify html

--- app.py
+++ app.py
@@ -5,18 +5,28 @@ app = Flask(__name__)
 
 GAMES = {
     "大樂透": (1, 49, 6),
-    "今彩539": (1, 39, 5),
-    "powerball": (1, 69, 5)  # 這裡可以加入更多遊戲
+    "今彩 539": (1, 39, 5),
+    "四星彩": (0, 9, 4),
+    "三星彩": (0, 9, 3),
+    "威力彩": (1, 38, 5),
+    "bingo 五星": (1, 80, 5)  # 這裡可以加入更多遊戲
 }
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     numbers = []
     game = request.form.get("game")
+
     if request.method == "POST" and game in GAMES:
         start, end, count = GAMES[game]
         numbers = random.sample(range(start, end + 1), count)
-    return render_template("index.html", games=GAMES.keys(), numbers=numbers)
+        
+        if game == "威力彩":
+            second_zone = random.choice(range(1, 9))  # 第二區隨機選 1~8
+            numbers.append(second_zone)
+
+    return render_template("index.html", games=GAMES.keys(), numbers=numbers, game=game)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
